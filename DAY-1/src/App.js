d6c6190009f581be71fee025ef1f0a7253ff1778
cmharini23 <118423449+cmharini23@users.noreply.github.com>
2023-12-13 15:05:11 +0530

    Add files via upload

---
+++ DAY-1/src/App.js
@@ -0,0 +1,15 @@
+// File: App.js
+
+import React from 'react';
+import SignupPage from './components/SignupPage'; // Import the SignupPage component
+import './App.css'; // Import the CSS file for styling
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <SignupPage />
+    </div>
+  );
+};
+
+export default App;
